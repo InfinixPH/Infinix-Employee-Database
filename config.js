d25ca2fef0e48f69acdaa73a39c531f7ec0fbd79
InfinixPH <infinixph.00@gmail.com>
2026-05-17 14:45:20 +0800

    Update config.js

--- config.js
+++ config.js
@@ -2,5 +2,5 @@
 // CONFIG — gitignored. DO NOT commit this file.
 // Copy config.example.js → config.js and fill in your values.
 // ============================================================
-const CLIENT_ID = '995259716921-k5m9g081spimdf6ph3gpermr4d7t0fr6.apps.googleusercontent.com';
+const CLIENT_ID = '995259716921-brln332lr863pmtvb48rdvh3uqfbjsfo.apps.googleusercontent.com';
 const SHEET_ID  = '1OJ8SMQFbC3iGxPvYRAqXwKYXIYyMuufEa_oQNcPWaMY';
