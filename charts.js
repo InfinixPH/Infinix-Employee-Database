ab932e0e6095622705b48089750b0adff2a84516
InfinixPH <infinixph.00@gmail.com>
2026-05-18 14:30:52 +0800

    Update charts.js

--- charts.js
+++ charts.js
@@ -75,7 +75,7 @@ function renderDashboard(){
       .catch(()=>{});
   }
 
-  const deployPct=activeSheetRows.length?Math.round(deployed/activeSheetRows.length*100):0;
+  const deployPct=activeSheetRows.length?Math.floor(deployed/activeSheetRows.length*100):0;
   const scanPct=activeTotal?Math.round(scanned/activeTotal*100):0;
   const reqPct=activeTotal?Math.round(reqComplete/activeTotal*100):0;
 
